26f22d6728f8025c38fe6d8d017aab58bc3a803d
riccardo <riccardo.colombi.studenti@isii.it>
2020-11-06 10:07:07 +0100

    Aggiungere i file di progetto.

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PicturBox
+{
+    public partial class frmMain : Form
+    {
+        int stato = 0;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (stato == 0)
+            {
+                pictureBox1.Image = Properties.Resources.macchinarossa;
+                stato = 1;
+            }
+            else
+            {
+                pictureBox1.Image = Properties.Resources.macchinabianca;
+                stato = 0;
+            }
+        }
+    }
+}
